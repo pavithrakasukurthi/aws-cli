17ab34d1373b50bac6d0c09e85b15c33c2041d9e
Pavithra <pavithrakasukurthi00@gmail.com>
2026-04-02 22:49:31 +0530

    creating ec2 and updating route 53 records editing 2

--- script1.sh
+++ script1.sh
@@ -5,7 +5,7 @@
 for instance in $@
 do
     echo "Creating $instance..."
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-03b68898754661942 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value="$instance"}]' --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-03b68898754661942 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${instance}}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ "$instance" == "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
@@ -14,8 +14,10 @@ do
     fi
 
     #updating Route 53 records
+     
+     ROCORD_NAME="${instance}.pavithra.sbs"
 
-    aws route53 change-resource-record-sets \
+    aws route53 change-resource-record-sets \ 
   --hosted-zone-id Z0034753Q3D37U6HFEYZ \
   --change-batch '
   {
