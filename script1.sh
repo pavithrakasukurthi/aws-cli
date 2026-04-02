4bca3e05728ed15bd15363989716e9f17b6be2ec
Pavithra <pavithrakasukurthi00@gmail.com>
2026-04-02 13:37:16 +0530

    creating ec2 and updating route 53 records

---
+++ script1.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#creating instances
+
+for instance in $@
+do
+    echo "Creating $instance..."
+    INSTANCE_ID=aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-03b68898754661942 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query 'Instances[0].InstanceId' --output text
+
+    if [ "$instance" == "frontend" ]; then
+        IP=aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text
+    else
+        IP=aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text
+    fi
+
+    #updating Route 53 records
+
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id Z0034753Q3D37U6HFEYZ \
+  --change-batch '
+  {
+    "Comment": "Updating record set"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$RECORD_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
+  '
+
+done
